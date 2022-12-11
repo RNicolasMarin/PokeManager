84564036f7b1b36094af1e64b1d5ec3ebb1ceab8
Nicolas Marin <nmarin@pdssa.com.ar>
2022-12-11 15:27:53 -0300

    Setting up Navigation and ListPokeSpeciesFragment added

---
+++ app/src/main/java/com/pokemanager/ui/species/ListPokeSpeciesFragment.kt
@@ -0,0 +1,18 @@
+package com.pokemanager.ui.species
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pokemanager.R
+
+class ListPokeSpeciesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_list_poke_species, container, false)
+    }
+}
