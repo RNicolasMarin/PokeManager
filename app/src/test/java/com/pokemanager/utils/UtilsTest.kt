4aa10a71c17f377e25b83d5c3c3a3563d84de0dd
Nicolas Marin <nmarin@pdssa.com.ar>
2023-03-19 22:07:57 -0300

    Test for getTotalStepsAtDownloadingAll added

--- app/src/test/java/com/pokemanager/utils/UtilsTest.kt
+++ app/src/test/java/com/pokemanager/utils/UtilsTest.kt
@@ -1,11 +1,17 @@
 package com.pokemanager.utils
 
 import com.google.common.truth.Truth.*
+import com.pokemanager.utils.Utils.getTotalStepsAtDownloadingAll
 import org.junit.Test
 
 class UtilsTest {
 
-
-
+    //getTotalStepsAtDownloadingAll
+    @Test
+    fun `getTotalStepsAtDownloadingAll correct result`() {
+        val actual = getTotalStepsAtDownloadingAll()
+        val expected = 16
+        assertThat(expected).isEqualTo(actual)
+    }
 
 }
