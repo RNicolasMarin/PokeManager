be6e92c5e650b23c1d3ee4f86f63b5e5de34d131
Nicolas Marin <nmarin@pdssa.com.ar>
2023-05-14 13:25:02 -0300

    Image to shows while loading an actual image changed to the app's icon

--- app/src/main/java/com/pokemanager/utils/AndroidUtils.kt
+++ app/src/main/java/com/pokemanager/utils/AndroidUtils.kt
@@ -18,7 +18,7 @@ object AndroidUtils {
 
     fun loadImage(root: View, imageUrl: String, pokeSpecieDetailImage: ImageView) {
         Glide.with(root).load(imageUrl)
-            .placeholder(R.drawable.ic_launcher_background)//image while the image is loading
+            .placeholder(R.mipmap.ic_launcher_foreground)//image while the image is loading
             //.diskCacheStrategy(DiskCacheStrategy.ALL)//in case it's having problems when loading the image
             .into(pokeSpecieDetailImage)
     }
